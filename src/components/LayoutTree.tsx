e893da720d304a36fafe0d52f620e6a31cf52bef
Cindy Michelle <cindy@kodefox.com>
2025-04-04 01:45:30 +0700

    fix bullet position

--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -266,7 +266,7 @@ const LayoutTree = () => {
         }}
       >
         <TransitionGroup>
-          <BulletCursorOverlay isHighlighted={activeThought?.isCursor} x={activeThought?.x} y={activeThought?.y} />
+          <BulletCursorOverlay isCursorActive={activeThought?.isCursor} x={activeThought?.x} y={activeThought?.y} />
           {treeThoughtsPositioned.map((thought, index) => (
             <TreeNode
               {...thought}
