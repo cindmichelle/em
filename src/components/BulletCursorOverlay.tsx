cd63377ddc907bdfb43642acebecccf27729c2a5
Cindy Michelle <cindy@kodefox.com>
2025-06-16 09:07:19 +0700

    works well in desktop safari mozilla edge

--- src/components/BulletCursorOverlay.tsx
+++ src/components/BulletCursorOverlay.tsx
@@ -3,12 +3,15 @@ import { useSelector } from 'react-redux'
 import { css } from '../../styled-system/css'
 import Path from '../@types/Path'
 import SimplePath from '../@types/SimplePath'
+import { isMac, isSafari } from '../browser'
 import useBulletPosition from '../hooks/useBulletPosition'
 import attributeEquals from '../selectors/attributeEquals'
 import isContextViewActive from '../selectors/isContextViewActive'
 import rootedParentOf from '../selectors/rootedParentOf'
 import head from '../util/head'
 import parentOf from '../util/parentOf'
+
+const isOSXSafari = isMac && isSafari()
 
 /** A larger circle that surrounds the bullet of the cursor thought. */
 const BulletCursorOverlay = ({
@@ -89,46 +92,78 @@ const BulletCursorOverlay = ({
     (isTableCol1 ? 0 : bulletSpanLeftPosition) +
     contextBreadcrumbXPadding
 
+  // berhasil
   // calculate X position of overlay by adding y position + top position of bullet + extra y position when context breadcrumb is exists
-  const translateY = y - glyphMarginBottom + contextBreadcrumbYPadding
+
+  // Rounding down bulletSvgMarginBottom specifically for macOS Safari, because top positioning doesn’t respect subpixel values,
+  // whereas in the Bullet SVG, subpixel values still affect position calculation.
+  // The solution is to apply only the rounded-down bulletSvgMarginBottom for the CSS top positioning,
+  // and then apply the remaining subpixel value to the CursorOverlaySVG’s margin-bottom instead.
+  const bulletSvgMarginBottom = isOSXSafari ? Math.floor(glyphMarginBottom) : glyphMarginBottom
+  const translateY = y - bulletSvgMarginBottom + contextBreadcrumbYPadding
+
+  const overlaySvgMarginBottom = isOSXSafari ? glyphMarginBottom - bulletSvgMarginBottom : undefined
+  const top = translateY
+  const remainder = glyphMarginBottom - bulletSvgMarginBottom
+  console.log(
+    `BulletCursorOverlay: translateY=${translateY}, top=${top}, remainder= ${glyphMarginBottom} - ${Math.floor(glyphMarginBottom)} =${remainder}`,
+  )
 
   return (
-    <svg
-      className={css({
-        // Safari has a known issue with subpixel calculations, especially during animations and with SVGs.
-        // This caused the bullet slide animation to end with a jerky movement.
-        // By setting "will-change: transform;", we hint to the browser that the transform property will change in the future,
-        // allowing the browser to optimize the animation.
-        willChange: 'transform',
-        ...(isCursorActive
-          ? {
-              fillOpacity: 1,
-              transition: `transform {durations.veryFast} ease-in-out`,
-            }
-          : null),
-      })}
-      viewBox='0 0 600 600'
+    <div
       style={{
-        height: lineHeight,
-        width: lineHeight,
+        lineHeight: 2,
+        // height: 36,
+        // backgroundColor: 'blue',
         position: 'absolute',
-        transform: `translateY(${translateY}px) translateX(${translateX}px)`,
+        // transform: `translateY(${translateY}px) translateX(${translateX}px)`,
+        // transition: `transform 80ms ease-in-out`,
+        willChange: !isOSXSafari ? 'transform' : undefined,
+
+        left: `${translateX}px`,
+        top: `${translateY}px`,
+        transition: `left 80ms ease-in-out, top 80ms ease-in-out`,
       }}
-      ref={svgElement}
     >
-      <g>
-        <ellipse
-          ry={bulletOverlayRadius}
-          rx={bulletOverlayRadius}
-          cy='300'
-          cx='300'
-          className={css({
-            fillOpacity: 0.25,
-            fill: 'fg',
-          })}
-        />
-      </g>
-    </svg>
+      <svg
+        className={css({
+          // Safari has a known issue with subpixel calculations, especially during animations and with SVGs.
+          // This caused the bullet slide animation to end with a jerky movement.
+          // By setting "will-change: transform;", we hint to the browser that the transform property will change in the future,
+          // allowing the browser to optimize the animation.
+          // verticalAlign: 'baseline',
+          ...(isCursorActive
+            ? {
+                fillOpacity: 1,
+                // transition: `transform {durations.veryFast} ease-in-out`,
+              }
+            : null),
+        })}
+        viewBox='0 0 600 600'
+        style={{
+          height: lineHeight,
+          width: lineHeight,
+          backgroundColor: 'red',
+          // position: 'absolute',
+          // transform: `translateY(${translateY}px) translateX(${translateX}px)`,
+          marginBottom: overlaySvgMarginBottom,
+        }}
+        ref={svgElement}
+      >
+        <g>
+          <ellipse
+            ry={bulletOverlayRadius}
+            rx={bulletOverlayRadius}
+            cy='300'
+            cx='300'
+            className={css({
+              fillOpacity: 0.25,
+              fill: 'fg',
+            })}
+          />
+        </g>
+      </svg>
+    </div>
   )
 }
 
